6ede7e8894ecb082acee7c6f99e3d93efaa39653
Cesar Gonzalez <gcesar13@yahoo.com>
2022-12-04 18:14:01 -0700

    form componenet start:

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -1,6 +1,33 @@
 import { Box, Typography, useTheme, useMediaQuery} from "@mui/material"
+import Form from "./Form";
 
-const LoginPage = () => {
-    return ( <div>loginPage</div> )
+const LoginPage = () => { 
+    const theme = useTheme();
+    const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+
+    return (<Box>
+        <Box width="100%" backgroundColor={theme.palette.background.alt} p="1rem 6%" textAlign="center">
+         <Typography
+          fontWeight="bold"
+          fontSize="32px"
+          color="primary"
+        >
+          Sociopedia
+        </Typography>
+        </Box>
+
+        <Box width={isNonMobileScreens}
+            p="2rem"
+            m="2rem auto"     
+            borderRadius="1.5rem"
+            backgroundColor={theme.palette.background.alt}
+        >
+            <Typography fontWeight="500" varient="h5" sx={{ mb: "1.5rem"}}>
+                Welcome, This is a mock social media page
+            </Typography>
+            <Form/>
+        </Box>
+    </Box>
+    )
 }
 export default LoginPage;
