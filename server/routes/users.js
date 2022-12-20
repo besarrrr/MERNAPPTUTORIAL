a327946a741bedd64ef17d136af59205ea87c13a
Cesar Gonzalez <gcesar13@yahoo.com>
2022-12-19 20:05:23 -0700

    test

--- server/routes/users.js
+++ server/routes/users.js
@@ -16,4 +16,7 @@ router.get("/:id/friends", verifyToken, getUserFriends);
 /* UPDATE */
 router.patch("/:id/:friendId", verifyToken, addRemoveFriend);
 
-export default router;
+export default router;     
+
+
+
